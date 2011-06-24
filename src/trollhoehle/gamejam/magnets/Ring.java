7f8f822415b871af0dd2caf13322c1c2db30471f
moritz <moritz.winter@gmail.com>
2011-06-24 19:26:46 +0200

    some changes at Ring

--- src/trollhoehle/gamejam/magnets/Ring.java
+++ src/trollhoehle/gamejam/magnets/Ring.java
@@ -1,6 +1,8 @@
 package trollhoehle.gamejam.magnets;
 
 import org.newdawn.slick.Image;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.geom.Circle;
 import org.newdawn.slick.geom.Shape;
 
 /**
@@ -11,10 +13,8 @@ import org.newdawn.slick.geom.Shape;
  */
 public class Ring extends Entity {
 
-    public Ring(float posX, float posY, Shape shape, Image img) {
-
-	// TODO: directly create shape and image here (maybe)
-	super(posX, posY, shape, img);
+    public Ring(float posX, float posY, float radius) throws SlickException {
+	super(posX, posY, new Circle(posX, posY, radius), new Image("res/images/testRing.png"));
     }
 
     public Obstacle[] update(float timePerFrame, float toCenterX, float toCenterY, float attract) {
