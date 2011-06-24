76da4ec557a5b6dbb1fa9a42ea94dc768c6345d5
Kai Franke <kaifrankes@googlemail.com>
2011-06-25 01:32:12 +0200

    updated obstacle collision

--- src/trollhoehle/gamejam/magnets/Obstacle.java
+++ src/trollhoehle/gamejam/magnets/Obstacle.java
@@ -17,6 +17,12 @@ public class Obstacle extends PhysicalEntity {
     public Obstacle(float posX, float posY, int hp, float startSpeed) throws SlickException {
 	super(posX, posY, new Circle(posX, posY, 20), new Image("res/images/trollface.png"), hp, startSpeed);
     }
+public Obstacle[] collision(Entity collider) {
+	if(collider instanceof Player || collider instanceof Ring) {
+		super.collision(collider);	
+	}
+	return null;
+}
     
     
 }
