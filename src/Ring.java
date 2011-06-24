d55ca6fe94f964f029f579793d56fc173e1b7c8e
moritz <moritz.winter@gmail.com>
2011-06-24 02:09:08 +0200

    Tests

--- src/Ring.java
+++ src/Ring.java
@@ -1,11 +1,13 @@
-import org.newdawn.slick.Image;
+
 
 
 public class Ring extends PhysicalEntity {
-	protected Image ring = null;
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	public Ring(float x, float y, float radius) {
 		super(x , y, radius, 0);
-		
 	}
 
 }
