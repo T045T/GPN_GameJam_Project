c30614fc00c2443e58f42186401ea7c15ee785e1
Cakemix Troll <mholoch@gmail.com>
2011-06-25 21:46:11 +0200

    fixed the powerup-image-bug

--- src/trollhoehle/gamejam/magnets/Powerup.java
+++ src/trollhoehle/gamejam/magnets/Powerup.java
@@ -4,10 +4,10 @@ import org.newdawn.slick.Image;
 
 import org.newdawn.slick.SlickException;
 
-public class Powerup extends Obstacle {
+public abstract class Powerup extends Obstacle {
 
     public Powerup(float posX, float posY, int hp, float startSpeed, Image img) throws SlickException {
-	super(posX, posY, hp, startSpeed);
+	super(posX, posY, hp, startSpeed, img);
 	// TODO Auto-generated constructor stub
     }
 
