2b1a25d4f49fa9b7fb3c9f8235f1b7cde715ef53
moritz <moritz.winter@gmail.com>
2011-06-25 02:11:07 +0200

    some speed changes

--- src/trollhoehle/gamejam/magnets/Powerup.java
+++ src/trollhoehle/gamejam/magnets/Powerup.java
@@ -5,7 +5,7 @@ import org.newdawn.slick.SlickException;
 public class Powerup extends Obstacle {
 
 	public Powerup(float posX, float posY, int hp, float startSpeed) throws SlickException {
-		super(posX, posY, hp, startSpeed);
+		super(posX, posY, hp, startSpeed, 0.08f);
 		// TODO Auto-generated constructor stub
 	}
 
