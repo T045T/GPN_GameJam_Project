070b2b9638c6165527d324f98a1bf88880874934
moritz <moritz.winter@gmail.com>
2011-06-25 23:22:29 +0200

    blah

--- src/trollhoehle/gamejam/magnets/Management.java
+++ src/trollhoehle/gamejam/magnets/Management.java
@@ -8,7 +8,6 @@ import java.util.ArrayList;
 
 import org.newdawn.slick.AppGameContainer;
 import org.newdawn.slick.BasicGame;
-import org.newdawn.slick.Color;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Image;
